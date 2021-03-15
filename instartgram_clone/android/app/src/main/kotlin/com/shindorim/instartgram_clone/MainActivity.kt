2dde3c0eb5285c6fb492d7266e5b5ceecb48c641
shinaaaa <shin029abc@gmail.com>
2021-03-15 11:12:16 +0900

    init instartgram clone

---
+++ instartgram_clone/android/app/src/main/kotlin/com/shindorim/instartgram_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shindorim.instartgram_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ instartgram_clone/lib/main.dart
@@ -0,0 +1,19 @@
+/// "Dart"의 처음 실행되는 부분
+import 'package:flutter/material.dart';
+
+void main() {
+  // Flutter App 시작되는 부분
+  runApp(MyApp());
+}
+
+/// StatelessWidget -> 위젯의 상태 변화가 없다.
+/// StatefulWidget -> 위젯의 상태 변화가 있다.
+class MyApp extends StatelessWidget {
+  /// 위젯이 생성되자마자 실행
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
