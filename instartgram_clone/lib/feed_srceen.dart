b89201bc6577445d6b48c03b3ec25475e43a717b
shinaaaa <shin029abc@gmail.com>
2021-03-22 23:31:43 +0900

    ListView

--- instartgram_clone/lib/feed_srceen.dart
+++ instartgram_clone/lib/feed_srceen.dart
@@ -7,12 +7,17 @@ class feedScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           "Instagram",
-          style: TextStyle(
-            fontFamily: "VeganStyle",
-            color: Colors.black87
-          ),
+          style: TextStyle(fontFamily: "VeganStyle", color: Colors.black87),
         ),
       ),
+      body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 30),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.accents[index % Colors.accents.length],
+      height: 100,
     );
   }
 }
