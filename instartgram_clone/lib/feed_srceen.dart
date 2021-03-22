045ebce7f8bc299e91342e21175b41e51018e4d5
shinaaaa <shin029abc@gmail.com>
2021-03-22 21:44:29 +0900

    폰트 추가

--- instartgram_clone/lib/feed_srceen.dart
+++ instartgram_clone/lib/feed_srceen.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class feedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Instagram",
+          style: TextStyle(
+            fontFamily: "VeganStyle"
+          ),
+        ),
+      ),
+    );
   }
 }
