e6f6fcef2c4dfc23c13c7d069cb326fc5413d7e5
shinaaaa <shin029abc@gmail.com>
2021-03-23 21:42:33 +0900

    CircularProgressIndicator

--- instartgram_clone/lib/widgets/post.dart
+++ instartgram_clone/lib/widgets/post.dart
@@ -3,21 +3,37 @@ import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
   final int index;
+  Size size;
 
-  const Post(this.index, {
+  Post(
+    this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (size == null) size = MediaQuery.of(context).size;
+
     return CachedNetworkImage(
       imageUrl: 'https://picsum.photos/id/$index/200/200',
+      placeholder: (BuildContext context, String url) {
+        return Container(
+            width: size.width,
+            height: size.width,
+            child: SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.black87,
+                )));
+      },
       imageBuilder: (BuildContext context, ImageProvider imageProvider) {
-        return AspectRatio(aspectRatio: 1,
+        return AspectRatio(
+          aspectRatio: 1,
           child: Container(
             decoration: BoxDecoration(
-                image: DecorationImage(image: imageProvider, fit: BoxFit.cover)
-            ),
+                image:
+                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
           ),
         );
       },
