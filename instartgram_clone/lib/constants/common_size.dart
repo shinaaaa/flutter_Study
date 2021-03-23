d306d0bf6ab529b47b078a0d238c1ba8d473266e
shinaaaa <shin029abc@gmail.com>
2021-03-23 22:15:55 +0900

    post header

---
+++ instartgram_clone/lib/constants/common_size.dart
@@ -0,0 +1,5 @@
+const double common_gap = 14.0;
+const double common_xxs_gap = 8.0;
+
+
+const double avatar_size = 30.0;
--- instartgram_clone/lib/widgets/post.dart
+++ instartgram_clone/lib/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instartgram_clone/constants/common_size.dart';
 
 import 'my_progress_indicator.dart';
 
@@ -16,6 +17,36 @@ class Post extends StatelessWidget {
   Widget build(BuildContext context) {
     if (size == null) size = MediaQuery.of(context).size;
 
+    return Column(
+      children: <Widget>[_postImage(), _postHeader()],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: ClipOval(
+            child: CachedNetworkImage(
+              imageUrl: 'https://picsum.photos/100',
+              width: avatar_size,
+              height: avatar_size,
+            ),
+          ),
+        ),
+        Expanded(child: Text('username')),
+        IconButton(
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.black87,
+            ),
+            onPressed: () {})
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
       imageUrl: 'https://picsum.photos/id/$index/200/200',
       placeholder: (BuildContext context, String url) {
