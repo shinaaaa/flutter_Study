7c204b1ddfe8ee13b379c19b3f2ccb8e8ad8ea20
shinaaaa <shin029abc@gmail.com>
2021-03-23 21:32:50 +0900

    Image extra Library

--- instartgram_clone/lib/widgets/post.dart
+++ instartgram_clone/lib/widgets/post.dart
@@ -1,18 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
   final int index;
 
-  const Post(
-    this.index, {
+  const Post(this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover)
+            ),
+          ),
+        );
+      },
     );
   }
 }
