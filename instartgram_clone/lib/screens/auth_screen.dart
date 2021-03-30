4cf20cac79aee3c4cef77304bc36944e192ba280
shinaaaa <shin029abc@gmail.com>
2021-03-30 22:23:20 +0900

    FlatButton shape Border

--- instartgram_clone/lib/screens/auth_screen.dart
+++ instartgram_clone/lib/screens/auth_screen.dart
@@ -17,18 +17,36 @@ class _AuthScreenState extends State<AuthScreen> {
       FadeStack(
         selectForm: selectForm,
       ),
-      Container(
-          child: FlatButton(
-        onPressed: () {
-          setState(() {
-            if (selectForm == 0)
-              selectForm = 1;
-            else
-              selectForm = 0;
-          });
-        },
-        child: Text("go to sign"),
-      ))
+      Positioned(
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 40,
+        child: Container(
+            color: Colors.white,
+            child: FlatButton(
+              shape: Border(top: BorderSide(color: Colors.grey)),
+              onPressed: () {
+                setState(() {
+                  if (selectForm == 0)
+                    selectForm = 1;
+                  else
+                    selectForm = 0;
+                });
+              },
+              child: RichText(
+                text: TextSpan(
+                    text: (selectForm == 0 ? "계정이 없으신가요? " : "계정이 있으신가요? "),
+                    style: TextStyle(color: Colors.black45),
+                    children: [
+                      TextSpan(
+                          text: (selectForm == 0 ? "가입하기" : "로그인"),
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold))
+                    ]),
+              ),
+            )),
+      )
     ])));
   }
 }
